af69cf48b020954fdb0a281cb5b8d414caa0015e
wongsitu <sa_waika_4d_23@hotmail.com>
2023-02-27 19:19:06 -0700

    feat: added alternative map

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,7 @@ import { format } from '../../utils/format';
 import useDebounce from '../../hooks/useDebounce';
 
 const Home = () => {
+  const [showTrafficIncidents, setShowTrafficInicidents] = useState(false);
   const [state, setState] = useState<Range[]>([
     {
       startDate: new Date(),
@@ -29,7 +30,7 @@ const Home = () => {
   return (
     <div className="relative">
       <div
-        className="absolute p-4 bg-white top-0 right-0"
+        className="absolute p-4 bg-white top-0 right-0 flex flex-col justify-center"
         style={{ zIndex: 999 }}
       >
         <DateRange
@@ -39,6 +40,15 @@ const Home = () => {
           ranges={state}
           maxDate={new Date()}
         />
+        {/* <button
+          type="button"
+          className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow"
+          onClick={() => setShowTrafficInicidents((state) => !state)}
+        >
+          {showTrafficIncidents
+            ? 'Hide traffic incidents'
+            : 'Show traffic incidents'}
+        </button> */}
       </div>
       <div>
         <MapContainer center={position} zoom={13}>
@@ -46,6 +56,12 @@ const Home = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          {showTrafficIncidents && (
+            <TileLayer
+              attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+              url="https://api.mapbox.com/styles/v1/wongsitu/clenl5gww000501ptpyjwe8mw/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid29uZ3NpdHUiLCJhIjoiY2xlZXhtOGk1MDVzZDN0cG10a2x1NjUxMCJ9.hFkKaWzhPkDlzmcDSBKj-w"
+            />
+          )}
           <MarkerClusterGroup chunkedLoading>
             {locations.map((el, idx) =>
               el.properties.latitude && el.properties.longitude ? (
